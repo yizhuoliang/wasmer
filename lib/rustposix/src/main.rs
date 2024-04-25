ed2179eadb4174c97396d777b06375e58f83b572
Yizhuo Liang <yizhuo.liang.hello@gmail.com>
2024-04-24 21:26:59 -0400

    Small modification

--- lib/rustposix/src/main.rs
+++ lib/rustposix/src/main.rs
@@ -2,11 +2,11 @@ use libc::STDOUT_FILENO;
 use rustposix::*;
 
 fn main() {
-    lind_lindrustinit(0);
+    lind_lindrustinit(0); // should be put to Store instantiate
     lind_rustposix_thread_init(1, 0);
-    let my_string = "Hello, world!";
+    let my_string = "Hello, world!\n";
     let c_buf = my_string.as_ptr() as *const libc::c_void;
     let buf_len = my_string.len();
-    lind_write(STDOUT_FILENO, c_buf, buf_len, 1);
+    lind_write(STDOUT_FILENO, c_buf, buf_len, 1); // fd_write
     lind_lindrustfinalize();
 }
