b5bd6f581aa5053c6053f58e74329b5c08bbbd6f
Yizhuo Liang <yizhuo.liang.hello@gmail.com>
2024-04-29 17:02:05 -0400

    use WasmPtr

--- lib/wasix/src/syscalls/lind_syscalls/lind_write.rs
+++ lib/wasix/src/syscalls/lind_syscalls/lind_write.rs
@@ -7,7 +7,7 @@ use crate::syscalls::*;
 
 pub fn lind_write<M: MemorySize>(
     mut ctx: FunctionEnvMut<'_, WasiEnv>,
-    iovs: i32,
+    iovs: WasmPtr<M::Offset, M>,
     len: i32,
 ) -> Result<Errno, WasiError> {
     // Here, we should fetch the pid aka cageid aka wasiEnv id,
@@ -17,10 +17,11 @@ pub fn lind_write<M: MemorySize>(
     lind_rustposix_thread_init(1, 0);
     let mut env = ctx.data();
     let memory = unsafe { env.memory_view(&ctx) };
-    let buf_addr = iovs as u64 + memory.data_ptr() as u64;
+    let offset = iovs.offset().into() as u64;
+    let base = memory.data_ptr() as u64;
     let ptr: *const libc::c_void = unsafe {
         // Cast u64 to usize, then usize to *const libc::c_void
-        buf_addr as usize as *const libc::c_void
+        (offset + base) as usize as *const libc::c_void
     };
     lind_write_inner(1, ptr, len as usize, 1);
     lind_lindrustfinalize();
