d08bf079df8ef3b92ecb75ad1ace9a4c63f97534
Yizhuo Liang <yizhuo.liang.hello@gmail.com>
2024-04-29 14:07:37 -0400

    try another?

--- lib/wasix/src/syscalls/lind_syscalls/lind_write.rs
+++ lib/wasix/src/syscalls/lind_syscalls/lind_write.rs
@@ -6,12 +6,15 @@ use crate::syscalls::*;
 pub fn lind_write<M: MemorySize>(
     mut ctx: FunctionEnvMut<'_, WasiEnv>,
     iovs: i32,
-    iovs_len: M::Offset,
+    len: i32,
 ) -> Result<Errno, WasiError> {
     // Here, we should fetch the pid aka cageid aka wasiEnv id,
     // but I'm suprised that there's no "ID" field for WasiEnv
     // let mut env = ctx.data();
-    // lind_write_inner(1, buf, count, 1);
     lind_lindrustinit(0);
+    let mut env = ctx.data();
+    let memory = unsafe { env.memory_view(&ctx) };
+    let buf_addr = iovs as i64 + memory;
+    lind_write_inner(1, buf_addr, len, 1);
     Ok(Errno::Success)
 }
