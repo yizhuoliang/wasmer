5c8f0e654d29065d9a5c383ef2cdd977c8d83fa0
Yizhuo Liang <yizhuo.liang.hello@gmail.com>
2024-04-29 00:47:19 -0400

    adjust n args in lind write (stiil bull sht)

--- lib/wasix/src/syscalls/lind_syscalls/lind_write.rs
+++ lib/wasix/src/syscalls/lind_syscalls/lind_write.rs
@@ -5,10 +5,8 @@ use crate::syscalls::*;
 
 pub fn lind_write<M: MemorySize>(
     mut ctx: FunctionEnvMut<'_, WasiEnv>,
-    fd: WasiFd,
     iovs: WasmPtr<__wasi_ciovec_t<M>, M>,
     iovs_len: M::Offset,
-    nwritten: WasmPtr<M::Offset, M>,
 ) -> Result<Errno, WasiError> {
     // Here, we should fetch the pid aka cageid aka wasiEnv id,
     // but I'm suprised that there's no "ID" field for WasiEnv
