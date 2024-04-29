bd96e46207ef798a2d40498f7a6264ae3b8c0f4a
Yizhuo Liang <yizhuo.liang.hello@gmail.com>
2024-04-29 15:20:46 -0400

    finish lind_write demo

--- lib/wasix/src/syscalls/lind_syscalls/lind_write.rs
+++ lib/wasix/src/syscalls/lind_syscalls/lind_write.rs
@@ -1,4 +1,4 @@
-use rustposix::{lind_lindrustinit, lind_write_inner};
+use rustposix::{lind_lindrustinit, lind_rustposix_thread_init, lind_write_inner};
 
 use super::*;
 use crate::syscalls::*;
@@ -12,9 +12,14 @@ pub fn lind_write<M: MemorySize>(
     // but I'm suprised that there's no "ID" field for WasiEnv
     // let mut env = ctx.data();
     lind_lindrustinit(0);
+    lind_rustposix_thread_init(1, 0);
     let mut env = ctx.data();
     let memory = unsafe { env.memory_view(&ctx) };
-    let buf_addr = iovs as i64 + memory;
-    lind_write_inner(1, buf_addr, len, 1);
+    let buf_addr = iovs as i64 + memory.data_ptr() as i64;
+    let ptr: *const libc::c_void = unsafe {
+        // Cast i64 to usize, then usize to *const libc::c_void
+        buf_addr as usize as *const libc::c_void
+    };
+    lind_write_inner(1, ptr, len as usize, 1);
     Ok(Errno::Success)
 }
