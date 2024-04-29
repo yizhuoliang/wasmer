1bd8bb8dc1f6b2a1686ee828a81d44d84535ffd8
Yizhuo Liang <yizhuo.liang.hello@gmail.com>
2024-04-29 16:12:12 -0400

    add finalize first

--- lib/wasix/src/syscalls/lind_syscalls/lind_write.rs
+++ lib/wasix/src/syscalls/lind_syscalls/lind_write.rs
@@ -1,4 +1,6 @@
-use rustposix::{lind_lindrustinit, lind_rustposix_thread_init, lind_write_inner};
+use rustposix::{
+    lind_lindrustfinalize, lind_lindrustinit, lind_rustposix_thread_init, lind_write_inner,
+};
 
 use super::*;
 use crate::syscalls::*;
@@ -11,6 +13,7 @@ pub fn lind_write<M: MemorySize>(
     // Here, we should fetch the pid aka cageid aka wasiEnv id,
     // but I'm suprised that there's no "ID" field for WasiEnv
     // let mut env = ctx.data();
+    lind_lindrustfinalize();
     lind_lindrustinit(0);
     lind_rustposix_thread_init(1, 0);
     let mut env = ctx.data();
