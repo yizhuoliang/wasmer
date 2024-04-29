e492659845c86a7551cd6eb1c66c155303360fef
Yizhuo Liang <yizhuo.liang.hello@gmail.com>
2024-04-29 16:19:45 -0400

    move finalize

--- lib/wasix/src/syscalls/lind_syscalls/lind_write.rs
+++ lib/wasix/src/syscalls/lind_syscalls/lind_write.rs
@@ -13,7 +13,6 @@ pub fn lind_write<M: MemorySize>(
     // Here, we should fetch the pid aka cageid aka wasiEnv id,
     // but I'm suprised that there's no "ID" field for WasiEnv
     // let mut env = ctx.data();
-    lind_lindrustfinalize();
     lind_lindrustinit(0);
     lind_rustposix_thread_init(1, 0);
     let mut env = ctx.data();
@@ -24,5 +23,6 @@ pub fn lind_write<M: MemorySize>(
         buf_addr as usize as *const libc::c_void
     };
     lind_write_inner(1, ptr, len as usize, 1);
+    lind_lindrustfinalize();
     Ok(Errno::Success)
 }
